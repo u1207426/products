b7cc3355810e0b5987ed2c81e1963acc10096b5c
李宏君 <homlee@rihiroshikuntekiMacBook-Pro.local>
2019-12-16 22:51:23 +0800

    refactor

--- products.py
+++ products.py
@@ -1,36 +1,52 @@
 # 二維清單
 import os
 
-products = [] #大清單
 # 讀取檔案
-if os.path.isfile('products.csv'): # 檢查檔案在不在
-	print('找到檔案了！')
-	with open('products.csv', 'r', encoding='utf-8') as f:
+def read_file(filename):
+	products = [] #大清單
+	with open(filename, 'r', encoding='utf-8') as f:
 		for line in f:
 			if '商品,價格' in line:
 				continue # 跳到下一迴圈
 			name, price = line.strip().split(',') #split切割完的結果是清單,分別存到name price
 			products.append([name, price])
-	print(products)
-else:
-	print('找不到檔案...')
+	return products
 
 # 讓數用者輸入
-while True:
-	name = input('請輸入商品名稱： ')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格： ')
-	price = int(price)
-	products.append([name, price])
-print(products)
+def user_input(products):
+	while True:
+		name = input('請輸入商品名稱： ')
+		if name == 'q':
+			break
+		price = input('請輸入商品價格： ')
+		price = int(price)
+		products.append([name, price])
+	print(products)
+	return products
 
 # 印出所有購買紀錄
-for p in products:
-	print(p[0], '的價格是', p[1])
+def print_products(products):
+	for p in products:
+		print(p[0], '的價格是', p[1])
 
 # 寫入檔案
-with open('products.csv', 'w', encoding='utf-8') as f:
-	f.write('商品,價格\n')
-	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n') # +只能字串與字串合併
+def write_file(filename, products):
+	with open(filename, 'w', encoding='utf-8') as f:
+		f.write('商品,價格\n')
+		for p in products:
+			f.write(p[0] + ',' + str(p[1]) + '\n') # +只能字串與字串合併
+
+# 主要的執行程式碼通常會寫成main function，是程式的進入點
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename): # 檢查檔案在不在
+		print('找到檔案了！')
+		products = read_file(filename)
+	else:
+		print('找不到檔案...')
+
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)
+
+main()
